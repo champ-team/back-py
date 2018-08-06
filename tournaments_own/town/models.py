14fa2d84507a00f02c8400830d37af9ab372ce1c
Alexey Shumakov <bobshaftoe@yandex.ru>
2018-08-07 02:19:37 +0300

    crate models(partial)

--- tournaments_own/town/models.py
+++ tournaments_own/town/models.py
@@ -3,3 +3,23 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=255)
+
+
+class Tournament(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+class Game(models.Model):
+    map_name = models.CharField(max_length=255)
+    server_name = models.CharField(max_length=255)
+    server_password = models.CharField(max_length=255)
+    start_time = models.DateTimeField()
+
+
+class TeamStat(models.Model):
+    standing = models.TextField()
+
+
+class PlayerStat(models.Model):
+    kills = models.IntegerField()
