2c68e94f98a2062fc815fa45f72ef1252b33cc8d
Alexey Shumakov <bobshaftoe@yandex.ru>
2018-08-12 00:48:12 +0300

    fix user create

--- tournaments_own/town/serializers.py
+++ tournaments_own/town/serializers.py
@@ -1,12 +1,28 @@
 from django.contrib.auth.models import User
 from tournaments_own.town.models import Team, TeamStat, Tournament, Game, PlayerStat
 from rest_framework import serializers
-
+from rest_framework.validators import UniqueValidator
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        write_only=True,
+        validators=[UniqueValidator(queryset=User.objects.all())])
+    username = serializers.CharField(
+        write_only=True,
+        validators=[UniqueValidator(queryset=User.objects.all())])
+
+    password = serializers.CharField(min_length=8)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data['username'],
+            validated_data['email'],
+            validated_data['password'])
+        return user
+
     class Meta:
         model = User
-        fields = ( 'username',)
+        fields = ('username', 'password', 'email')
 
 
 class TeamSerializer(serializers.ModelSerializer):
